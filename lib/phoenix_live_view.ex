46421448b8274e7fc66b532fa52fb64391f78435
John Curran <jcurran996@gmail.com>
2023-10-16 13:52:57 -0500

    Fix markdown typo

--- lib/phoenix_live_view.ex
+++ lib/phoenix_live_view.ex
@@ -2074,7 +2074,7 @@ defmodule Phoenix.LiveView do
   the key passed to `start_async/3`.
 
   The underlying process will be killed with the provided reason, or
-  {:shutdown, :cancel}`. if no reason is passed. For `assign_async/3`
+  `{:shutdown, :cancel}`. if no reason is passed. For `assign_async/3`
   operations, the `:failed` field will be set to `{:exit, reason}`.
   For `start_async/3`, the `handle_async/3` callback will receive
   `{:exit, reason}` as the result.
